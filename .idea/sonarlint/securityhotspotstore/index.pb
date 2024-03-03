434691f77e75dcb628201b1c128832f493ccad29
Filip Drażba <fdrazba@gmail.com>
2024-03-03 12:59:46 +0100

    1

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,13 @@
 
-W
-'src/main/java/pl/edu/pb/wi/fd/Main.java,8/6/868cbbf956a1fbb8c0f4f47d42c2720d8ca1fd90
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+_
+/src/main/java/pl/edu/pb/wi/fd/MyClientMain.java,1/5/151fc9d020d5b7096dfb9dd0f1f4382718438a8f
+^
+.src/main/java/pl/edu/pb/wi/fd/MyServerInt.java,e/1/e10460fe20b3d582d13381488163009efebbdd44
+?
+security.policy,b/4/b4bc80d3f427c7d2e7fb403008548fe65054cf4b
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/main/java/pl/edu/pb/wi/fd/MyClientMain.java
+++ src/main/java/pl/edu/pb/wi/fd/MyClientMain.java
@@ -1,2 +1,23 @@
-package pl.edu.pb.wi.fd;public class MyClientMain {
+package pl.edu.pb.wi.fd;
+
+import java.math.BigDecimal;
+import java.rmi.Naming;
+
+public class MyClientMain {
+    public static void main(String[] args) {
+        System.setProperty("java.security.policy", "security.policy");
+        System.setSecurityManager(new SecurityManager());
+        try {
+            MyServerInt myRemoteObject = (MyServerInt) Naming.lookup("//localhost/ABC");
+
+            Double firstValue = 1d;
+            Double secondValue = 2d;
+            Double result = myRemoteObject.add(firstValue, secondValue);
+
+            System.out.println("Wysłano do servera: " + firstValue + " + " + secondValue);
+            System.out.println("Otrzymana z serwera odpowiedź: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
